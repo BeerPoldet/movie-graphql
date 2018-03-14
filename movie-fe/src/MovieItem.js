763a57743a02a448bfddc2f8b3cd5a7905f0d4c1
Poldet <beer@pupasoft.com>
2018-03-14 21:38:08 +0700

    Remove code

--- movie-fe/src/MovieItem.js
+++ movie-fe/src/MovieItem.js
@@ -4,7 +4,6 @@ import gql from 'graphql-tag'
 const MovieItem = ({ movie }) => (
   <li>
     {movie.name} | {movie.rating}
-    <ActorList actors={movie.actors} />
   </li>
 )
 
@@ -14,7 +13,6 @@ export const MovieItemFragments = {
       id
       name
       rating
-      ...ActorList
     }
   `
 }
